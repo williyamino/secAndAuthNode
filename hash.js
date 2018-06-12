42d43c27795e9346f55047b0f75926409d91aa15
vasylvasylyshyn <vasylyshyn1984@gmail.com>
2018-06-13 00:32:32 +0300

    Added bcrypt to project and create login route. Added compare password method

---
+++ hash.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+bcrypt.genSalt(10,(err,salt)=>{
+    if(err) return next(err);
+
+    bcrypt.hash('123456', salt, (err,hash)=>{
+        if(err) return next(err);
+        console.log(hash);
+    })
+});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
 
 
@@ -22,6 +21,21 @@ app.post('/api/user',(req,res)=>{
     user.save((err,doc)=>{
         if(err) res.status(400).send(err)
         res.status(200).send(doc)
+    })
+});
+
+app.post('/api/user/login',(req,res)=>{
+
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) res.json({message:'Auth failed, user not found'});
+
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(err) throw err;
+            if(!isMatch) return res.status(400).json({
+                message:'Wrong password'
+            })
+            res.status(200).send(isMatch);
+        });
     })
 });
 
