428f20e65c6ecab66fd803fd8107d3264ad3a296
vasylvasylyshyn <vasylyshyn1984@gmail.com>
2018-06-12 23:25:59 +0300

    Added user model and schema. Create route to post user

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+   email:{
+       type:String,
+       required:true,
+       trim:true,
+       unique:1
+   },
+   password:{
+       type:String,
+       required:true,
+       minLength:6
+   }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
